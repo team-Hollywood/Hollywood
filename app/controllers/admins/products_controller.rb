68dcea619ee32a1b155847f37a16e8f407d4e248
vagrant <vagrant@localhost.localdomain>
2020-07-05 03:03:01 +0000

    add admin_products2

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -21,9 +21,16 @@ class Admins::ProductsController < ApplicationController
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to admins_product_path(@product)
+    else
+      render :edit
+    end
   end
 
   private
