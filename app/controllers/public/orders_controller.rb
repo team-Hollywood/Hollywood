c5e40663752f42f18933f1e8de3d36508e338d0a
vagrant <vagrant@localhost.localdomain>
2020-07-07 02:43:00 +0000

    customer_order_history

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -9,9 +9,11 @@ class Public::OrdersController < ApplicationController
   end
 
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   def create
