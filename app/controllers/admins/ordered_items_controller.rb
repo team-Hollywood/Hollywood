fc9e1496c9031a43a5feee6b27bae426933a5146
vagrant <vagrant@localhost.localdomain>
2020-07-11 07:13:44 +0000

    add admins_order_item_sutatus

--- app/controllers/admins/ordered_items_controller.rb
+++ app/controllers/admins/ordered_items_controller.rb
@@ -2,15 +2,21 @@ class Admins::OrderedItemsController < ApplicationController
    before_action :authenticate_admin!
 
   def update
-  	@ordered_item = OrderedItem.find(params[:id])
-  	if @ordered_item.update(ordered_items_params)
-  		redirect_to admins_orders_path
-  	else
-  		render 'orders/show'
-  	end
+    @ordered_item = OrderedItem.find(params[:id])
+    @ordered_item.update(ordered_items_params)
+    @order = Order.find(@ordered_item.order_id)
+    if    @ordered_item.production_status == "製作中"
+          @order.update(order_status: 2)
+    end
+    if  @order.ordered_items.where(production_status: 3).count == @order.ordered_items.count
+        @order.update(order_status: 3)
+    end
+    redirect_to admins_orders_path
   end
+
   private
 	def ordered_items_params
 	  params.require(:ordered_item).permit(:production_status)
 	end
+
 end
--- app/models/ordered_item.rb
+++ app/models/ordered_item.rb
@@ -3,6 +3,6 @@ class OrderedItem < ApplicationRecord
   belongs_to :product
 
   enum production_status: {
-  	着手不可: 0,制作待ち: 1,製作中: 2,制作完了: 3
+  	着手不可: 0,製作待ち: 1,製作中: 2,製作完了: 3
   }
 end
